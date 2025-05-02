26c734a98879e7b99fb89f9b0e6e9c57389a8fe0
avc105 <avc105@tpu.ru>
2025-05-03 00:11:40 +0700

    Сделал реализацию сервиса SpaceSettingsService

---
+++ src/main/java/org/example/services/SpaceSettingsServiceImpl.java
@@ -0,0 +1,36 @@
+package org.example.services;
+
+import lombok.AllArgsConstructor;
+import org.example.domain.dto.SpaceSettingsDto;
+import org.example.domain.entities.SpaceSettingsEntity;
+import org.example.mappers.Mapper;
+import org.example.repositories.SpaceSettingsRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SpaceSettingsServiceImpl implements SpaceSettingsService {
+    private final SpaceSettingsRepository spaceSettingsRepository;
+    private final Mapper<SpaceSettingsEntity, SpaceSettingsDto> mapper;
+    @Override
+    public SpaceSettingsDto saveSpaceSettings(SpaceSettingsDto spaceSettingsDto, UserDetails user) {
+        return mapper.mapToDto(spaceSettingsRepository.save( mapper.mapFromDto(spaceSettingsDto)));
+    }
+
+    @Override
+    public SpaceSettingsDto updateSpaceSettings(SpaceSettingsDto spaceSettingsDto, UserDetails user) {
+        return mapper.mapToDto(spaceSettingsRepository.save( mapper.mapFromDto(spaceSettingsDto)));
+    }
+
+    @Override
+    public SpaceSettingsDto findSpaceSettingsById(Long id) {
+        return mapper.mapToDto(spaceSettingsRepository.findById(id).orElse(null));
+    }
+
+    @Override
+    public void removeSpaceSettingsById(Long id) {
+        spaceSettingsRepository.deleteById(id);
+    }
+}
