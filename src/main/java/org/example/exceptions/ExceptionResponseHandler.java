847299f9c038ad34ad305b4affa45f73de450433
Maxim <therikomero@gmail.com>
2025-05-17 03:24:25 +0700

    fix import

--- src/main/java/org/example/exceptions/ExceptionResponseHandler.java
+++ src/main/java/org/example/exceptions/ExceptionResponseHandler.java
@@ -1,7 +1,7 @@
 package org.example.exceptions;
 
-import com.example.taskManagementSystem.domain.dto.responses.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.example.domain.dto.responses.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
